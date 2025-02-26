7413c73822fcb0e1a04faeeff9b2410505ebf890
EDVAN ALVES DE OLIVEIRA <25132698@CFP127ALUNO.LOCAL>
2025-02-26 11:24:57 -0300

    Métodos get e sets iniciados, encapsulamento, calculo de Imc e outras correções

--- src/br/senai/sp/jandira/clinica/Clinica.java
+++ src/br/senai/sp/jandira/clinica/Clinica.java
@@ -9,36 +9,29 @@ public class Clinica {
 		System.out.println("Hellowww salve mundo");
 
 		Paciente paciente1 = new Paciente();
-		paciente1.name = "Bob";
-		paciente1.height = 1.65;
-		paciente1.weight = 55;
+		paciente1.setName("Bo");
+		paciente1.setHeight(1.65);
+		paciente1.setWeight(55);
 		paciente1.phone = "Não tem celular" ;
 		paciente1.birthDate = LocalDate.of(2000, 2, 19);
 		
-		paciente1.showPacientProfile();
+		paciente1.showPatientProfile();
 		
 		
 		Paciente paciente2 = new Paciente();
-		paciente2.name = "Sou Paciente Não";
-		paciente2.height = 1.70;
-		paciente2.weight = 59;
-		paciente2.phone = "11 40028922" ;
+		paciente2.setName("Edvan");
+		paciente2.setHeight(1.70);
+		paciente2.setWeight(59);
+		paciente2.phone = "(11)4002-8922";
 		paciente2.birthDate = LocalDate.of(2002, 9, 23);
 
-		paciente2.showPacientProfile();
+		paciente2.showPatientProfile();
 		
 		Paciente paciente3 = new Paciente();
 		Paciente paciente4 = new Paciente();
 		Paciente paciente5 = new Paciente();
 		Paciente paciente6 = new Paciente();
 		Paciente paciente7 = new Paciente();
-		
-		System.out.println(paciente1);
-		System.out.println(paciente2);
-
-
-		
-		//creating a patient object
 		
 		
 	}
--- src/br/senai/sp/jandira/clinica/model/Paciente.java
+++ src/br/senai/sp/jandira/clinica/model/Paciente.java
@@ -2,41 +2,112 @@ package br.senai.sp.jandira.clinica.model;
 
 import java.time.LocalDate;
 
+/*
+ * Programador: Edvan
+ * Data: 26/02/2025
+ * Empresa: SENAI
+ */
+
+
 public class Paciente{
 		
-	public String name;
+	private String name;
 	public LocalDate birthDate;
 	public int weight;
-	public double height;
+	private double height;
 	public String phone;
+	private double imc;
+	
+	public void setName(String name) {
+		
+		if (name.length() >= 3){
+			this.name = name.toUpperCase();
+		}
+		else {			
+			System.out.println("O nome " + name + " deve conter pelo menos 3 caracteres");
+		}
+		
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setHeight(double height) {
+		if (height <= 0) {
+			//Numero invalido
+			System.out.println("Altura "+ height + " do(a) paciente"+ name + " inválida \nO programa não aceita valores menores que 0.");
+		}
+		else if (height < 1) {
+			//Abaixo do range
+			System.out.println("Altura "+ height + " do(a) paciente" + name + " inválida \n O mínimo é 1 metro.");
+		}
+		
+		else if (height > 3) {
+			//Acima do Range
+			System.out.println("Altura "+ height + " do(a) paciente" + name + " inválida \n O mínimo é 1 metro.");
+			
+		}
+		
+		else if (height >= 1 && height <= 3) {
+			//TRUE (Está dentro do range)
+			this.height = height;
+		}
+		else {
+			System.out.println("Altura inválida.");
+		}
+	}
+	
+	public double getHeight() {
+		return this.height;
+	}
 	
 	
+	public void setWeight(int weight) {
+		if (weight < 30) {
+			//Abaixo do Range
+			System.out.println("Peso "+ weight + " do(a) paciente" + name + " inválido, o programa só aceita a partir de 30kg.");
+		}
+		else if (weight > 500) {
+			//Acima do Range
+			System.out.println("Peso "+ weight + " do(a) paciente" + name + " inválido, o programa só aceita abaixo de 500kg.");
+		}
+		else {
+			//TRUE
+			this.weight = weight;
+		}
+	}
 	
-	public void calcAge() {
-		int age;
+	public int getWeight() {
+		return this.weight;
+	}
+	
+	public void calculateAge() {
+		
 		
 		
 	}
 	
-	public void calculateImc() {
-		
-		
-		
+	private double calculateImc() {
+		imc = (weight/ (Math.pow(height, 2)));
+		return imc;
 	}
 	
 	public void classifyImc() {
 		
 	}
 	
-	public void showPacientProfile() {
-		System.out.println("===================");
+	public void showPatientProfile() {
+		calculateImc();
+		System.out.println("=======================");
 		System.out.println("FICHA DO PACIENTE");
-		System.out.println("===================");
+		System.out.println("=======================");
 		System.out.println("Nome: "+ name);
 		System.out.println("Peso: "+ weight);
 		System.out.println("Altura: "+ height);
+		System.out.println("IMC: "+ String.format("%.2f", calculateImc()));
 		System.out.println("Telefone: "+ phone);
-		System.out.println("===================");
+		System.out.println("=======================");
 		System.out.println();
 		
 	}
